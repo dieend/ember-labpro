0c377dc4bc3d17b4cb33bb9f2faa8dd80b3d75c0
Muhammad Adinata <mail.dieend@gmail.com>
2013-04-16 18:21:29 +0700

    add drawable manager dan custom view

--- src/com/labpro/game/ember/CustomView.java
+++ src/com/labpro/game/ember/CustomView.java
@@ -2,12 +2,17 @@ package com.labpro.game.ember;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class CustomView extends View{
 
 	public CustomView(Context context) {
 		super(context);
+	}
+	public CustomView(Context context, AttributeSet attr) {
+		super(context, attr);
+		
 	}
 	@Override
 	public void onDraw(Canvas c) {
--- src/com/labpro/game/ember/DrawableManager.java
+++ src/com/labpro/game/ember/DrawableManager.java
@@ -11,16 +11,33 @@ public class DrawableManager{
 	 * construct pake aplication context
 	 * @param context
 	 */
-	public DrawableManager(Context context) {
+	private DrawableManager(Context context) {
 		// TODO ganti gambar
 		Resources r = context.getResources();
 		emberBitmap = BitmapFactory.decodeResource(r, R.drawable.bucket);
-		
+	}
+	/**
+	 * must be called only once
+	 * @param context
+	 */
+	public static void initInstance(Context context) {
+		assert instance == null;
+		instance = new DrawableManager(context);
+	}
+	/**
+	 * must have called initInstance() before
+	 * @return
+	 */
+	public static DrawableManager getInstance() {
+		assert instance != null;
+		return instance;
 	}
 	public Bitmap getEmberBitmap(){
 		assert emberBitmap != null;
 		return emberBitmap;
 	}
+	
+	private static DrawableManager instance;
 	private Bitmap emberBitmap;
 	
 }
